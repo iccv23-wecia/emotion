c054067bc3120553624d5949697a5ac542e79f55
iccv23-wecia <132705674+iccv23-wecia@users.noreply.github.com>
2023-09-11 15:29:46 +0300

    Update main.py

--- evaluation_script/main.py
+++ evaluation_script/main.py
@@ -63,7 +63,7 @@ def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwarg
         ground_truth_emo.append(emo2idx[ground_truth_emotion])
     acc = accuracy.compute(references=ground_truth_emo, predictions=test_emo)['accuracy']
     f1 = f1_score.compute(references=ground_truth_emo, predictions=test_emo,average="macro")['f1']
-    print(f1,"f1")
+    # print(f1,"f1")
     output = {}
     if phase_codename == "dev":
         print("Evaluating for Dev Phase")
